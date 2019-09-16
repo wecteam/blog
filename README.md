475ffe29a0bfc06fd67ad3cd91bac91c4d3ef524
liuhui188 <liuhui312@jd.com>
2019-09-16 16:00:07 +0800

    '自动生成readme'

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# blog
 
-- [中文博客排版指南](docs/document-guide.md)
+# 目录
+
+* [【译】在生产环境中使用原生JavaScript模块](https://wecteam.io/undefined)
+
+* [Javascript抽象语法树下篇(实践篇)](https://wecteam.io/undefined)
+
+* [Javascript抽象语法树上篇(基础篇)](https://wecteam.io/undefined)
+
+* [CSS Houdini实现动态波浪纹](https://wecteam.io/undefined)
+
+* [碰撞检测的向量实现](https://wecteam.io/undefined)
+    
--- themes/icarus/scripts/index.js
+++ themes/icarus/scripts/index.js
@@ -12,7 +12,8 @@ require('../includes/helpers/layout')(hexo);
 require('../includes/helpers/override')(hexo);
 require('../includes/helpers/page')(hexo);
 require('../includes/helpers/site')(hexo);
-
+var fs = require("fs");
+var path = require("path");
 // Fix large blog rendering OOM
 const postHtmlFilter = hexo.extend.filter.list()['after_render:html'];
 for (let filter of postHtmlFilter) {
@@ -29,4 +30,28 @@ hexo.extend.helper.register('console', function () {
 hexo.extend.helper.register('post_key', function(path, customKey){
     customKey = customKey || new Buffer(path).toString('base64');
     return customKey;
+});
+
+hexo.extend.generator.register('archive', function(locals){
+    var posts = locals.posts.map(item=>{
+        return {
+            title:item.title,
+            date:item.date.unix()
+        }
+    });
+    console.log(posts.length,posts.sort,posts);
+    posts.sort((a,b)=>{
+        return b.date - a.date
+    });
+    var content = `
+# 目录
+
+${posts.map(item=>{
+    console.log(item.title);
+    return `* [${item.title}](https://wecteam.io/${encodeURIComponent(item.path)})`
+}).join('\n\n')}
+    `;
+    fs.writeFile(path.join(this.base_dir,'README.md'),content,data=>{
+        console.log(data);
+    })
 });
